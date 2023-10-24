d3aeb898a899c2b741f765b6252a39c90c777b3c
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-10-24 18:18:12 +0600

    Add files via upload

---
+++ NextdaySchedule/NextdaySchedule.php
@@ -0,0 +1,11 @@
+<?php
+
+echo"1.8.00am-10.00am=MAS-Q2<br>";
+echo"2.10.00am-12.00am=CN-Q3<br>";
+echo"3.2.00pm-3.30pm=SRE<br>";
+echo"4.Calculator Purchase<br>";
+echo"5.Tea Purchase<br>";
+echo"6.Brush Purchase<br>";
+
+
+?>
